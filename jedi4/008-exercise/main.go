6def6ce79eaf70fd3d3a8c98cfbb974b1952f2e1
Skylar Smith <skylarsmith@pop-os.localdomain>
2020-08-08 11:29:02 -0400

    jedi4 exercise 8

---
+++ jedi4/008-exercise/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"james_bond":      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		"moneypenny_miss": []string{`moneypenny_miss`, `James Bond`, `Literature`, `Computer Science`},
+		"no_dr":           []string{`no_dr`, `Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	for _, p := range m {
+		for i, phrase := range p {
+			fmt.Printf("%v, %v\n", i, phrase)
+		}
+	}
+}
