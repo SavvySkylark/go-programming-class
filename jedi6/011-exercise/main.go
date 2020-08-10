f30886951ee7955feff11f3bff0c6388a45d0cd4
Skylar Smith <skylarsmith@pop-os.localdomain>
2020-08-09 20:47:38 -0400

    jedi6 exercise 11

---
+++ jedi6/011-exercise/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(factorial(4, 1))
+}
+
+// 4! 4*3*2*1
+
+func factorial(f int, acc int) int {
+
+	if f <= 0 {
+		return acc
+	} else {
+		acc *= f
+		f--
+		return factorial(f, acc)
+	}
+}
