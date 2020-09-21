5f6cb17017064c5897d9001d608a14659588873b
Skylar Smith <skylarsmith@pop-os.localdomain>
2020-09-21 07:41:40 -0400

    jedi 8 exercise 1

--- jedi8/001-exercise/main.go
+++ jedi8/001-exercise/main.go
@@ -5,29 +5,35 @@ import (
 	"fmt"
 )
 
-type person struct {
-	First   string   `json:"First"`
-	Last    string   `json:"Last"`
-	Age     int      `json:"Age"`
-	Sayings []string `json:"Sayings"`
+type user struct {
+	First string
+	Age   int
 }
 
 func main() {
-	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
-	fmt.Println(s)
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
 
-	var persons []person
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
 
-	err := json.Unmarshal([]byte(s), &persons)
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	bs, err := json.Marshal(users)
 	if err != nil {
 		fmt.Println(err)
 	}
-	for i, person := range persons {
-		fmt.Println("Person #", i)
-		fmt.Println("\t", person.First, person.Last, person.Age)
-		for _, saying := range person.Sayings {
-			fmt.Println("\t\t", saying)
-		}
-	}
 
+	fmt.Println(string(bs))
 }
